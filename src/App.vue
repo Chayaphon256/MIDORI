61dca593d22225d3574bfd9aea95c010cc4b4346
Chayaphon256 <pium.vipant@gmail.com>
2021-03-12 22:55:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div id="left">
+      <b-navbar toggleable type="dark" variant="dark">
+        <b-navbar-brand href="/">MIDORI</b-navbar-brand>
+        <b-navbar-toggle target="navbar-toggle-collapse">
+          <template #default="{ expanded }">
+            <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+            <b-icon v-else icon="chevron-bar-down"></b-icon>
+          </template>
+        </b-navbar-toggle>
+        <b-collapse id="navbar-toggle-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item><router-link to="/about">Basket</router-link></b-nav-item>
+            <b-nav-item href="/login">Login</b-nav-item>
+            <b-nav-item href="/product">Product</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#left {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+
+#right {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: right;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div class="page-content">
+      <b-container>
+        <br />
+        <h1>Welcome to MIDORI</h1>
+        <b-img
+          src="https://tuemaster.com/wp-content/uploads/2020/05/583651-topic-ix-23-1024x512.jpg"
+        ></b-img>
+      </b-container>
+      <b-container>
+        <br />
+        <b-button href="/register" variant="primary">Sign in now</b-button>
+      </b-container>
+    </div>
+  </div>
+</template>
+<style>
+.page-content img {
+  max-width: 100%;
+  height: auto !important;
+}
+</style>
